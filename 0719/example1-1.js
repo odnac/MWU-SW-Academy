48058f6409a4df5cc250f3b7c4da3a4454a4c970
odnac <cancode777@gmail.com>
2022-07-18 14:20:03 +0900

    명령형, 선언형 프로그래밍 방식 1-1

---
+++ 0719/example1-1.js
@@ -0,0 +1,17 @@
+// 명령형
+function double(arr) {
+    let results = []
+    for(let i = 0; i < arr.length; i++) {
+        results.push(arr[i] * 2)
+    }
+    return results
+}
+
+document.querySelector('body').innerHTML = double([1, 2, 3]);
+
+// 선언형
+function double(arr) {
+    return arr.map(number => number * 2)
+}
+
+document.querySelector('body').innerHTML = double([3, 4, 5]);
