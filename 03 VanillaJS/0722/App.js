6ca1e328a8e6aa86ae3b0c52e0d6d32f7fac7f63
odnac <cancode777@gmail.com>
2022-07-22 13:36:25 +0900

    0722 deadline

---
+++ 03 VanillaJS/0722/App.js
@@ -0,0 +1,60 @@
+import todoList from './todoList.js'
+import todoComments from './todoComments.js'
+import { request } from './api.js'
+
+export default function App({ $app }) {
+    this.state = {
+        todos: [],
+        selectedTodo: null,
+        comments: []
+    }
+
+    this.setState = nextState => {
+        this.state = nextState
+        todolist.setState(this.state.todos)
+        todocomments.setState({
+            selectedTodo: this.state.selectedTodo,
+            comments: this.state.comments
+        })
+    }
+
+    const todolist = new todoList({
+        $target: $app,
+        initialState: this.todos,
+        onClick: id => {
+            const selectedTodo = this.state.todos.find(todo => todo.id === id)
+            this.setState({
+                ...this.state,
+                selectedTodo
+            })
+            
+            // 댓글 목록 불러오기
+            request('https://kdt.roto.codes/comments?todo.id=${id}', (comments) => {
+                this.setState({
+                    ...this.state,
+                    comments
+                })
+            })
+        }
+    })
+    
+    const todocomments = new todoComments({
+        $target: $app,
+        initialState: {
+            selectedTodo: this.state.selectedTodo,
+            comments: this.state.comments 
+        }
+    })
+
+    //todos 불러오기
+    this.init = () => {
+        request('https://kdt.roto.codes/todos', (todos) => {
+            this.setState({
+                ...this.state,
+                todos
+            })
+        })
+    }
+
+    this.init()
+}
---
+++ 03 VanillaJS/0722/todoList.js
@@ -0,0 +1,32 @@
+export default function todoList({ $target, initialState, onClick}){
+    const $element= document.createElement('div')
+    $target.appendChild($element)
+
+    this.state = initialState
+
+    this.setState = nextState => {
+        this.state = nextState
+        this.render()
+    }
+
+    this.render = () => {
+        if(Array.isArray(this.state)){
+            $element.innerHTML = `
+                <h1>Simple todoList</h1>
+                <ul>
+                ${this.state.map(({ id, text }) => `<li data-id="${id}">${text}</li>`).join('')}
+                </ul>
+            `
+
+            $element.querySelectorAll('li').forEach($li => {
+                $li.addEventListener('click', (e) => {
+                    const {id} = e.target.dataset
+                    onClick(parseInt(id, 10))
+                })
+            })
+        }
+    }
+
+    this.render()
+}
+
