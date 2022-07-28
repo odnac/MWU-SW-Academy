ee2bf5384a117e1a47a79e7c3b72b78a179bc36f
odnac <cancode777@gmail.com>
2022-07-28 11:55:04 +0900

    To do App (3) BP.4

--- 03 VanillaJS/0728/source/App.js
+++ 03 VanillaJS/0728/source/App.js
@@ -18,10 +18,23 @@ export default function App({
 
     new TodoForm({
         $target,
-        onSubmit: (content) => {
-            alert(`${content} 추가처리!`)
+        onSubmit: async (content) => {
+            await request(`/${this.state.username}`, {
+                method: 'POST',
+                body: JSON.stringify({
+                    content,
+                    isCompleted: false
+                })
+            })
+            await fetchTodos()
         }
     })
+
+    this.setState = nextState => {
+        this.state = nextState
+
+        todoList.setState(this.state.todos)
+    }
 
     const todoList = new TodoList({
         $target,
@@ -34,14 +47,17 @@ export default function App({
         }
     })
 
-    const init = async () => {
+    const fetchTodos = async () => {
         const { username } = this.state
 
         if(username) {
             const todos = await request(`/${username}`)
-            console.log(todos)
+            this.setState({
+                ...this.state,
+                todos
+            })
         }
     }
     
-    init()
+    fetchTodos()
 }
--- 03 VanillaJS/0728/source/api.js
+++ 03 VanillaJS/0728/source/api.js
@@ -2,7 +2,12 @@ export const API_END_POINT = 'https://mwu.roto-todo-api.programmers.co.kr'
 
 export const request = async (url, options = {}) => {
     try {
-        const res = await fetch(`${API_END_POINT}${url}`, options)
+        const res = await fetch(`${API_END_POINT}${url}`, {
+            ...options,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
         if(res.ok) { 
             const json = await res.json()    
             return json
