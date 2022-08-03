a567a3adca24b32331496cc26de80eff5e4bfeca
odnac <cancode777@gmail.com>
2022-08-03 10:40:14 +0900

    고양이 사진 검색기 (3) BP.1 : api 호출

--- 03 VanillaJS/0803/source/App.js
+++ 03 VanillaJS/0803/source/App.js
@@ -1,12 +1,18 @@
+import { request } from "./api.js";
 import Header from "./Header.js";
 import Keyword from "./Keyword.js";
 
 export default function App({ $target }){
+    this.state = {
+        keywords: []
+    }
     const header = new Header({ 
         $target, 
-        onKeywordInput: (keyword) => {
+        onKeywordInput: async (keyword) => {
             if(keyword.trim().length > 1) {
-                console.log(keyword)
+                const keywords = await request(`/search?q=${keyword}`)
+
+                console.log(keywords)
             }
         }
     })
