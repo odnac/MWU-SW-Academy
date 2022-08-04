655a645fd763562e419f2a15c565c990b0b9cb22
odnac <cancode777@gmail.com>
2022-08-04 09:34:25 +0900

    파일삭제

--- 03 VanillaJS/0803_2/source/Keyword.js
+++ 03 VanillaJS/0803_2/source/Keyword.js
@@ -1,4 +1,10 @@
-export default function Keyword({ $target, initialState, onKeywordInput, onEnter }) {
+export default function Keyword({
+    $target, 
+    initialState, 
+    onKeywordInput,
+    onEnter
+}) 
+{
     const $keyword = document.createElement('input')
     $keyword.className = 'Keyword'
 
@@ -9,13 +15,15 @@ export default function Keyword({ $target, initialState, onKeywordInput, onEnter
     this.setState = nextState => {
         this.state = nextState
         $keyword.value = this.state.value
+
     }
 
     $keyword.addEventListener('keyup', e => {
-        if(e.key === 'Enter') {
-            onEnter()
-        } else {
-            onKeywordInput(e.target.value)
+        if(e.key ==='Enter'){
+        e.preventDefault()
+        onEnter()
+        }else{
+        onKeywordInput(e.target.value)
         }
     })
 }
--- 03 VanillaJS/0803_2/source/api.js
+++ 03 VanillaJS/0803_2/source/api.js
@@ -1,11 +1,11 @@
-const END_POINT = 'https://mwu.roto-cat-search-api.programmers.co.kr/api/cats'
+const END_POINT = 'https://mwu.roto-cat-search-api.programmers.co.kr/api/cats' // 3번 영상 /api/cats
 
 export const request = async(url) => {
     try {
         const res = await fetch(`${END_POINT}${url}`)
 
-        if(!res.ok) {
-            throw new Error('API 호출에 실패했습니다.')
+        if(!res.ok){
+            throw new Error ('API 호출에 실패')
         }
 
         return await res.json()
