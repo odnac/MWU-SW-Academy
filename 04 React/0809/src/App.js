0a4727292165a0454801ec3845cfddfd34cfa965
odnac <cancode777@gmail.com>
2022-08-09 11:22:53 +0900

    React (2) React.memo useCallback Custom Hook

--- 04 React/0809/src/App.js
+++ 04 React/0809/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useRef, useState } from "react"
+import Box2 from "./components/Box2"
+import Checkbox from "./components/Checkbox";
+import useToggle from "./components/hooks/useToggle";
+import Checkbox2 from "./components/Checkbox2";
+import Box3 from "./components/Box3";
+import useHover from "./components/hooks/useHover";
+import useKeyPress from "./components/hooks/useKeyPress";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  /*
+     * React.memo
+     */
+
+    const [count, setCount] = useState(0)
+
+    return ( 
+      <div>
+        {count}
+        <button onClick={() => setCount(count + 1)}>+</button>
+        <Box2 />
+      </div> 
+    )
+
+
+    /*
+     * useCallback
+    */
+
+    // const [foodOn, setFoodOn] = useState(false)
+    // const [clothesOn, setClothesOn] = useState(false)
+    // const [shelterOn, setShelterOn] = useState(false)
+
+    // const foodChange = useCallback((e) => setFoodOn(e.target.checked), [])
+    // const clothesChange =useCallback((e) => setClothesOn(e.target.checked), [])
+    // const shelterChange =useCallback((e) => setShelterOn(e.target.checked), [])
+ 
+    // return ( 
+    //   <div>
+    //     <Checkbox label="Food" on={foodOn} onChange={foodChange}/>
+    //     <Checkbox label="Clothes" on={clothesOn}  onChange={clothesChange}/>
+    //     <Checkbox label="Shelter" on={shelterOn}  onChange={shelterChange}/>
+    //   </div>
+    // )
+
+
+    /*
+    * Custom Hook
+    */
+
+    // const [on, toggle] = useToggle()
+    // const [ref, isHover] = useHover()
+    // const keyPressed = useKeyPress("a")
+
+    // return (
+    //   <div>
+    //     <Checkbox2 checked={on} onChange={toggle} />
+        
+    //     {isHover ? 'hover' : 'mouseout'}
+    //     <Box3 ref={ref}/>
+
+    //     {keyPressed && "Pressed"}
+    //   </div>
+    // )
 }
 
 export default App;
---
+++ 04 React/0809/src/components/Checkbox2.js
@@ -0,0 +1,10 @@
+const Checkbox2 = ({ label = "Label", checked = false, onChange }) => {
+    return (
+        <label>
+            {label}
+            <input type="checkbox" defaultChecked={checked} onChange={onChange}/>
+        </label>
+    )
+}
+
+export default Checkbox2
