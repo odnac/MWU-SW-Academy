2b5d9d40000db4c86dab346bcb0d95385cd18ea0
odnac <cancode777@gmail.com>
2022-08-18 13:31:58 +0900

    React (6) 사용자 정의 Hook 연습하기 - useScroll

---
+++ 04 React/0815/src/hooks/useRafState.js
@@ -0,0 +1,18 @@
+import { useCallback, useRef } from "react"
+
+const useRefState = (initialState) => {
+    const frame = useRef(0)
+    const [state, setState] = useState(initialState)
+
+    const setRefState = useCallback((value) => {
+        cancelAnimationFrame(frame.current)
+
+        frame.current = requestAnimationFrame(() => {
+            setState(value)
+        })
+    }, [])
+
+    return [state, setRefState]
+}
+
+export default useRefState
