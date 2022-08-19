0688553e06104242afd1caf0d788fce91940768f
odnac <cancode777@gmail.com>
2022-08-19 10:42:50 +0900

    React (8) 컴포넌트 연습하기 - Modal

---
+++ 04 React/0817/src/stories/Modal.stories.js
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+export default {
+    title: 'Component/Modal',
+    component: Modal
+}
+
+export const Default = () => {
+    const [visible, setVisible] = useState(false)
+
+    return (
+        <div>
+            <button onClick={() => setVisible(true)}>Show Modal</button>
+            <Modal visible={visible} onClose={() => setVisible(false)}>Hi!</Modal>
+        </div>
+    )
+}
