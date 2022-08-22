dadbd84e72d5bbcb4e6e8ee44f00f82ab987bb37
odnac <cancode777@gmail.com>
2022-08-22 14:53:00 +0900

    React (9) 사용자 정의 Hook 연습하기 - useImperativeHandle

--- 04 React/0818/src/App.js
+++ 04 React/0818/src/App.js
@@ -3,18 +3,33 @@ import PostsPage from "./pages/PostsPage"
 import PostPage from "./pages/PostPage"
 import DefaultTemplate from "./components/template/DefaultTemplate"
 import NotFoundPage from "./pages/NotFoundPage"
+import Input from "./components/input";
+import { useRef } from "react";
 
 const App = () => {
 
+  /*  React (9) react-router : 페이지 이동
+   */
+  // return (
+  //   <DefaultTemplate>
+  //     <Routes>
+  //       <Route path="/" exact element={<h1>Home</h1>} />
+  //       <Route path="/posts" exact element={<PostsPage />} />
+  //       <Route path="/posts/:postId" element={<PostPage />} />
+  //       <Route path="*" element={<NotFoundPage />} />
+  //     </Routes>
+  //   </DefaultTemplate>
+  // )
+
+  /*  React (9) 사용자 정의 Hook 연습하기 - useImperativeHandle
+   */
+  const inputRef = useRef()
   return (
-    <DefaultTemplate>
-      <Routes>
-        <Route path="/" exact element={<h1>Home</h1>} />
-        <Route path="/posts" exact element={<PostsPage />} />
-        <Route path="/posts/:postId" element={<PostPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </DefaultTemplate>
+    <div>
+      <Input ref={inputRef}/>
+      <button onClick={() => inputRef.current.focus()}>Focus</button>
+      <button onClick={() => inputRef.current.clear()}>Clear</button>
+    </div>
   )
 }
 
---
+++ 04 React/0818/src/components/input.js
@@ -0,0 +1,21 @@
+import { useRef, useImperativeHandle, forwardRef } from "react"
+
+const Input = forwardRef((_, ref) => {
+    const inputRef = useRef()
+    useImperativeHandle(ref, () => ({
+        clear: () => {
+            inputRef.current.value = ''
+        },
+        focus: () => {
+            inputRef.current.focus()
+        }
+    }))
+
+    return (
+        <>
+            Input: <input ref={inputRef}/>
+        </>
+    )
+})
+
+export default Input
