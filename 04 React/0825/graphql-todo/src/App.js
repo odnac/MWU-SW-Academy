446d2504bab4ed5961d140563a37607ae17221d5
odnac <cancode777@gmail.com>
2022-08-30 17:40:25 +0900

    0825 - graphql-todo init

---
+++ 04 React/0825/graphql-todo/src/App.js
@@ -0,0 +1,25 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client"
+import styled from "@emotion/styled"
+import { Header } from "./components/Header";
+
+const Container = styled.div`
+  width: 400px;
+  margin: 0 auto;
+`
+
+const client = new ApolloClient ({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Container>
+        <Header>ToDo</Header>
+      </Container>
+    </ApolloProvider>
+  );
+}
+
+export default App;
