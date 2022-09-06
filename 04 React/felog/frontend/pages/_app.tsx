ffe9e3eebdaa976d3a03a2e9a9129e8aa0d7a734
odnac <cancode777@gmail.com>
2022-09-06 15:56:24 +0900

    React 심화 (5) felog 블로그 프로젝트 (3) 프론트엔드

--- 04 React/felog/frontend/pages/_app.tsx
+++ 04 React/felog/frontend/pages/_app.tsx
@@ -1,36 +1,17 @@
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
-import type { AppContext, AppProps } from 'next/app'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { AppShell, CoProvider } from '@co-design/core'
+import type { AppProps } from 'next/app'
 import { Header } from '../components'
-import { setContext } from '@apollo/client/link/context'
-import nookies from 'nookies'
-
-const httpLink = createHttpLink({
-  uri: 'http://localhost:1337/graphql',
-})
-
-const authLink = setContext((_, { headers }) => {
-  const { token } = nookies.get()
-
-  return {
-    headers: {
-      ...headers,
-      Authorizaion: token ? `Bearer ${token}` : '',
-    }
-  }
-})
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  //uri: 'http://localhost:1337/graphql',
-  cache: new InMemoryCache(),
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
 })
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const header = 
-    <AppShell.Header height={70}>
-      <Header token={pageProps.token}/>
-    </AppShell.Header>
+  const header = <AppShell.Header height={70}>
+    <Header />
+  </AppShell.Header>
 
   return (
     <ApolloProvider client={client}>
@@ -41,11 +22,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       </CoProvider>
     </ApolloProvider>
   )
-}
-
-MyApp.getInitailProps = async (appCtx: AppContext) => {
-  const { token } = nookies.get(appCtx.ctx)
-  return { pageProps: { token } }
 }
 
 export default MyApp
--- 04 React/felog/frontend/pages/index.tsx
+++ 04 React/felog/frontend/pages/index.tsx
@@ -3,52 +3,51 @@ import { Anchor, Card, Center, Container, Divider, EquallyGrid, Heading, Spinner
 import NextLink from 'next/link'
 
 const GET_POSTS = gql`
- query GetPost {
-   posts {
-     data {
-       id
-       attributes {
-         title
-         body
-         createdAt
-         user {
-           data {
-             attributes {
-               username
-             }
-           }
-         }
-       }
-     }
-   }
- }
+  query GetPosts {
+    posts {
+      data {
+        id
+        attributes {
+          title
+          body
+          createdAt
+          user {
+            data {
+              attributes {
+                username
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 `
 
-
 const Home = () => {
- const { data, loading, error } = useQuery(GET_POSTS)
+  const { data, loading, error } = useQuery(GET_POSTS)
 
- return (
-   <Container padding={16} co={{ marginTop: 16 }}>
-     {
-       loading ? <Center><Spinner/></Center> : (
-       <EquallyGrid cols={4}>
-         {data.posts.data.map((post: any) => (
-             <Card key={post.id}>
-               <NextLink href="/posts/[id]" as={`/posts/${post.id}`} passHref>
-                 <Anchor co={{ textDecoration: "none", color: "black" }}>
-                   <Heading level={4}>{post.attributes.title}</Heading>
-                 </Anchor>
-               </NextLink>
-               <Text lineClamp={3}>{post.attributes.body}</Text>
-               <Divider />
-               <Text block align="right">{post.attributes.user.data.attributes.username}</Text>
-             </Card>
-           ))}
-       </EquallyGrid>
-     )}
-   </Container>
- )
+  return (
+    <Container padding={16} co={{ marginTop: 16 }}>
+      {
+        loading ? <Center><Spinner/></Center> : (
+        <EquallyGrid cols={4}>
+          {data.posts.data.map((post: any) => (
+              <Card key={post.id}>
+                <NextLink href="/posts/[id]" as={`/posts/${post.id}`} passHref>
+                  <Anchor co={{ textDecoration: "none", color: "black" }}>
+                    <Heading level={4}>{post.attributes.title}</Heading>
+                  </Anchor>
+                </NextLink>
+                <Text lineClamp={3}>{post.attributes.body}</Text>
+                <Divider />
+                <Text block align="right">{post.attributes.user.data.attributes.username}</Text>
+              </Card>
+            ))}
+        </EquallyGrid>
+      )}
+    </Container>
+  )
 }
 
 export default Home
